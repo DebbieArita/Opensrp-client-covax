b2641bb33f4c255a5e26c1635d43364c3c6b6351
Debbie Mong'are <debbiearita96@gmail.com>
2022-05-27 02:52:04 +0300

    subject registration properties and json form

--- app/src/main/java/com/example/opensrp_client_covacs/activity/LoginActivity.java
+++ app/src/main/java/com/example/opensrp_client_covacs/activity/LoginActivity.java
@@ -1,4 +1,21 @@
 package com.example.opensrp_client_covacs.activity;
 
+import org.smartregister.view.activity.BaseLoginActivity;
+import org.smartregister.view.contract.BaseLoginContract;
+
 public class LoginActivity extends BaseLoginActivity implements BaseLoginContract.View {
+    @Override
+    protected int getContentView() {
+        return 0;
+    }
+
+    @Override
+    protected void initializePresenter() {
+
+    }
+
+    @Override
+    public void goToHome(boolean b) {
+
+    }
 }
