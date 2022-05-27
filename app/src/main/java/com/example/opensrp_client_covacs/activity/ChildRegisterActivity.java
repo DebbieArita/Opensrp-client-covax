d27f56e129a3ee44e1de7f4d537e35f5caa10d78
Debbie Mong'are <debbiearita96@gmail.com>
2022-05-27 07:00:06 +0300

    login implementation code WIP

---
+++ app/src/main/java/com/example/opensrp_client_covacs/activity/ChildRegisterActivity.java
@@ -0,0 +1,137 @@
+package com.example.opensrp_client_covacs.activity;
+
+import android.content.Intent;
+import android.view.MenuItem;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.opensrp_client_covacs.R;
+import com.example.opensrp_client_covacs.util.AppConstants;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.material.internal.NavigationMenu;
+import com.vijay.jsonwizard.constants.JsonFormConstants;
+
+import org.greenrobot.eventbus.EventBus;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.smartregister.AllConstants;
+import org.smartregister.child.activity.BaseChildRegisterActivity;
+import org.smartregister.child.domain.UpdateRegisterParams;
+import org.smartregister.child.util.ChildJsonFormUtils;
+import org.smartregister.child.util.Constants;
+import org.smartregister.child.util.Utils;
+import org.smartregister.client.utils.domain.Form;
+import org.smartregister.util.FormUtils;
+import org.smartregister.view.fragment.BaseRegisterFragment;
+
+import java.lang.ref.WeakReference;
+
+import timber.log.Timber;
+
+public class ChildRegisterActivity extends BaseChildRegisterActivity implements NavDrawerActivity {
+
+    private NavigationMenu navigationMenu;
+    private Fragment[] fragments;
+
+    @Override
+    protected Fragment[] getOtherFragments() {
+        ADVANCED_SEARCH_POSITION = 1;
+        fragments = new Fragment[1];
+        fragments[ADVANCED_SEARCH_POSITION - 1] = new WeakReference<>(new AdvancedSearchFragment()).get();
+        return fragments;
+    }
+
+    @Override
+    protected void onActivityResult() {
+        super.onActivityResult();
+    }
+
+
+
+    @Override
+    public String getRegistrationForm() {
+        return AppConstants.JsonForm.CHILD_ENROLLMENT;
+    }
+
+    @Override
+    protected void initializePresenter() {
+    }
+
+    @Override
+    protected BaseRegisterFragment getRegisterFragment() {
+        WeakReference<ChildRegisterFragment> weakReference = new WeakReference<>(new ChildRegisterFragment());
+        return weakReference.get();
+    }
+
+    @Override
+    protected void onResumption() {
+        super.onResumption();
+        WeakReference<ChildRegisterActivity> weakReference = new WeakReference<>(this);
+        navigationMenu = NavigationMenu.getInstance(weakReference.get());
+    }
+
+    @Override
+    public void openDrawer() {
+        if (navigationMenu != null) {
+            navigationMenu.openDrawer();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        EventBus.getDefault().unregister(this);
+        super.onPause();
+    }
+
+    @Override
+    public void startFormActivity(JSONObject jsonForm) {
+        Form form = new Form();
+        form.setWizard(false);
+        form.setHideSaveLabel(true);
+        form.setNextLabel("");
+
+        Intent intent = new Intent(this, Utils.metadata().childFormActivity);
+        String updatedForm = jsonForm.toString();
+        try {
+            updatedForm = FormUtils.obtainUpdatedForm(jsonForm, this);
+        } catch (JSONException e) {
+            Timber.e(e);
+        }
+        intent.putExtra(Constants.INTENT_KEY.JSON, updatedForm);
+        intent.putExtra(JsonFormConstants.JSON_FORM_KEY.FORM, form);
+        intent.putExtra(JsonFormConstants.PERFORM_FORM_TRANSLATION, true);
+        startActivityForResult(intent, ChildJsonFormUtils.REQUEST_CODE_GET_JSON);
+    }
+
+    @Override
+    protected void updateSearchItems(String barcodeSearchTerm) {
+        advancedSearchFormData.put(AppConstants.KeyConstants.ZEIR_ID, barcodeSearchTerm);
+        Fragment fragment = fragments[ADVANCED_SEARCH_POSITION - 1];
+        if (fragment instanceof AdvancedSearchFragment) {
+            AdvancedSearchFragment advancedSearchFragment = (AdvancedSearchFragment) fragment;
+            advancedSearchFragment.searchByOpenSRPId(barcodeSearchTerm);
+        }
+    }
+
+    @Override
+    public void finishActivity() {
+        finish();
+    }
+
+    @Override
+    protected void registerBottomNavigation() {
+        super.registerBottomNavigation();
+
+        MenuItem clients = bottomNavigationView.getMenu().findItem(R.id.action_clients);
+        if (clients != null) {
+            clients.setTitle(getString(R.string.header_children));
+        }
+        bottomNavigationView.getMenu().removeItem(R.id.action_library);
+    }
+
+    @Override
+    public void saveForm(String jsonString, UpdateRegisterParams updateRegisterParam) {
+        String jsonForm = AppUtils.validateChildZone(jsonString);
+        super.saveForm(jsonForm, updateRegisterParam);
+    }
+}
--- app/src/main/java/com/example/opensrp_client_covacs/activity/LoginActivity.java
+++ app/src/main/java/com/example/opensrp_client_covacs/activity/LoginActivity.java
@@ -1,21 +1,69 @@
 package com.example.opensrp_client_covacs.activity;
 
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.opensrp_client_covacs.R;
+import com.example.opensrp_client_covacs.application.CovacsApplication;
+import com.example.opensrp_client_covacs.presenter.LoginPresenter;
+import com.example.opensrp_client_covacs.util.AppConstants;
+
+
+import org.smartregister.growthmonitoring.service.intent.WeightForHeightIntentService;
+import org.smartregister.location.helper.LocationHelper;
+import org.smartregister.repository.AllSharedPreferences;
 import org.smartregister.view.activity.BaseLoginActivity;
 import org.smartregister.view.contract.BaseLoginContract;
 
 public class LoginActivity extends BaseLoginActivity implements BaseLoginContract.View {
+
+    private static final String WFH_CSV_PARSED = "WEIGHT_FOR_HEIGHT_CSV_PARSED";
+
+    @Override
+    protected void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mLoginPresenter.processViewCustomizations();
+        if (!mLoginPresenter.isUserLoggedOut()) {
+            goToHome(false);
+        }
+    }
+
     @Override
     protected int getContentView() {
-        return 0;
+        return R.layout.activity_login;
     }
 
     @Override
     protected void initializePresenter() {
-
+        mLoginPresenter = new LoginPresenter(this);
     }
 
     @Override
-    public void goToHome(boolean b) {
+    public void goToHome(boolean remote) {
+        if (remote) {
+            LocationHelper.getInstance().locationIdsFromHierarchy();
+            processWeightForHeightZScoreCSV();
+        }
 
+        if (mLoginPresenter.isServerSettingsSet()) {
+            Intent intent = new Intent(this, ChildRegisterActivity.class);
+            intent.putExtra(AppConstants.IntentKeyUtil.IS_REMOTE_LOGIN, remote);
+            startActivity(intent);
+        }
+
+        finish();
+    }
+
+    private void processWeightForHeightZScoreCSV() {
+        AllSharedPreferences allSharedPreferences = CovacsApplication.getInstance().getContext().allSharedPreferences();
+        if (!allSharedPreferences.getPreference(WFH_CSV_PARSED).equals("true")) {
+            WeightForHeightIntentService.startParseWFHZScores(this);
+            allSharedPreferences.savePreference(WFH_CSV_PARSED, "true");
+        }
     }
 }
