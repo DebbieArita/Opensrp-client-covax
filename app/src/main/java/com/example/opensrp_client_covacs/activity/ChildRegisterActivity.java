ad2df59f94392781f0e0eac2dece097452a96276
Debbie Mong'are <debbiearita96@gmail.com>
2022-07-15 11:49:35 +0300

    update nav drawer view

--- app/src/main/java/com/example/opensrp_client_covacs/activity/ChildRegisterActivity.java
+++ app/src/main/java/com/example/opensrp_client_covacs/activity/ChildRegisterActivity.java
@@ -49,7 +49,7 @@ public class ChildRegisterActivity extends BaseRegisterActivity implements Child
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_child_register); //to test
-//        NavigationMenu.getInstance(this, null, null);
+        NavigationMenu.getInstance(this);
     }
 
     @Override
